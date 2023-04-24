e4952c54204a5f96d32604f09f71fe094101d9b7
itzRaghav14 <98763689+itzRaghav14@users.noreply.github.com>
2023-04-24 18:05:09 +0530

    Update DSU.cpp

--- Templates/DSU.cpp
+++ Templates/DSU.cpp
@@ -11,6 +11,7 @@ public:
         parent.resize(N + 1);
         sizes.resize(N + 1);
         rank.resize(N + 1);
+        makeSet();
     }
     
     void merge(int child, int par){
@@ -19,7 +20,7 @@ public:
     }
     
     void makeSet(){
-        for(int i = 1; i <= N; i++){
+        for(int i = 0; i <= N; i++){
             parent[i] = i;
             sizes[i] = 1;
             rank[i] = 0;
