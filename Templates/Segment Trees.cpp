ea524cce32ad778e81e8960d99702a9a56bceb0e
itzRaghav14 <98763689+itzRaghav14@users.noreply.github.com>
2023-09-07 15:50:25 +0530

    Update Segment Trees.cpp

--- Templates/Segment Trees.cpp
+++ Templates/Segment Trees.cpp
@@ -1,72 +1,54 @@
 
 // === Segment Tree ===
+struct Node;
 
-struct Item{
-    long long sum;
-    int mx, mn;
-};
-
-template <typename T>
+template <typename T, typename Node = Node>
 class SegmentTree{
 private: 
-	
 	int size = 0;
-	vector<Item> seg;
-	
-    Item NEUTRAL_ELEMENT = {0, -MX, MX};
-	
-	Item single (int val) {
-		return {val, val, val};
-	}
-	
-	Item merge (Item left, Item right) {
-		Item node = NEUTRAL_ELEMENT;
-		node.sum = left.sum + right.sum;
-		node.mn = min(left.mn, right.mn);
-		node.mx = max(left.mx, right.mx);
-		return node;
-	}
-	
+	vector<Node> seg;
+		
     void build (int low, int high, int ind, vector<T> &arr) {
     	if(low == high) {
-    		seg[ind] = single(arr[low]);
+    		seg[ind] = Node(arr[low]);
     		return;
     	}
     	int mid = (low + high) / 2;
     	int leftInd = 2 * ind + 1, rightInd = 2 * ind + 2;
     	build(low, mid, leftInd, arr);
     	build(mid + 1, high, rightInd, arr);
-    	seg[ind] = merge(seg[leftInd], seg[rightInd]);
+    	seg[ind].merge(seg[leftInd], seg[rightInd]);
     }
 	
-	Item query (int low, int high, int ind, int left, int right) {
+	Node query (int low, int high, int ind, int left, int right) {
 		if(low > right || high < left) {
-			return NEUTRAL_ELEMENT;
+			return Node();
 		}
 		if(low >= left && high <= right) {
 			return seg[ind];
 		}
 		int mid = (low + high) / 2;
 		int leftInd = 2 * ind + 1, rightInd = 2 * ind + 2;
-		Item leftItem = query(low, mid, leftInd, left, right);
-		Item rightItem = query(mid + 1, high, rightInd, left, right);
-		return merge(leftItem, rightItem);
+		Node res;
+		Node leftItem = query(low, mid, leftInd, left, right);
+		Node rightItem = query(mid + 1, high, rightInd, left, right);
+		res.merge(leftItem, rightItem);
+		return res;
 	}
 	
 	void update (int low, int high, int ind, int index, int value) {
 		if(low == high) {
-			seg[ind] = single(value);
+			seg[ind] = Node(value);
 			return;
 		}
 		int mid = (low + high) / 2;
 		int leftInd = 2 * ind + 1, rightInd = 2 * ind + 2;
 		if(index <= mid) update(low, mid, leftInd, index, value);
 		else update(mid + 1, high, rightInd, index, value);
-		seg[ind] = merge(seg[leftInd], seg[rightInd]);
+		seg[ind].merge(seg[leftInd], seg[rightInd]);
 	}
 	
 public:
-	
 	SegmentTree () {
 		
 	}
@@ -86,11 +68,26 @@ public:
     	build(0, size - 1, 0, arr);
     }
     
-    Item query (int left, int right) {
+    Node query (int left, int right) {
     	return query(0, size - 1, 0, left, right);
     }
     
     void update (int index, int value) {
     	return update(0, size - 1, 0, index, value);
+    }
+};
+
+struct Node {
+    long long sum;
+    int mx, mn;
+    
+    Node () : sum(0), mx(-MX), mn(MX) {}
+    
+    Node (int val) : sum(val), mx(val), mn(val) {}
+    
+    void merge (Node &left, Node &right) {
+    	sum = left.sum + right.sum;
+    	mx = max(left.mx, right.mx);
+    	mn = min(left.mn, right.mn);
     }
 };
