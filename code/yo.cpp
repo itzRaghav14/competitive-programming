b8b07ffe262396b4b24b6a15a568323404df55ea
itzRaghav14 <98763689+itzRaghav14@users.noreply.github.com>
2023-09-26 15:25:37 +0530

    Create yo.cpp

---
+++ code/yo.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define dbg(...) ; 
+#define debug(...) ; 
+#define crndl ; 
+#endif
+
+void solve() {
+	
+	
+	
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+    freopen("io/input.txt", "r", stdin);
+    freopen("io/output.txt", "w", stdout);
+    freopen("io/error.txt", "w", stderr);
+    #endif
+	
+	int t = 1;
+	cin >> t;
+	
+	for(int _t = 1; _t <= t; _t++) {
+		debug(Testcase, _t);
+		solve();
+		crndl;
+	}
+	
+	return 0;
+}
