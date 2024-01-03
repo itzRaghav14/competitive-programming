50c69e7004e9a26033eb75145fd0eb8c8fccfb16
Raghav Goel <raghavgoel1453@gmail.com>
2024-01-03 17:16:07 +0530

    fixed debug.h

--- algo/debug.h
+++ algo/debug.h
@@ -39,7 +39,7 @@ template <class T> void _print(priority_queue <T> pq) {cerr << "[ "; while (!pq.
 template <class T> void _print(priority_queue<T, vector<T>, greater<T>> pq) {cerr << "[ "; while (!pq.empty()) {_print(pq.top()); cerr << " "; pq.pop();} cerr << "]";}
 template <class T, class V> void _print(T a[], V n) { cerr << "[ "; for(int i = 0; i < n; i++) { _print(a[i]); cerr << " "; } cerr << "]"; }
 template <class T, class V> void _print (T* a,V n, V m) { cerr << "[ "; for(int i = 0; i < n; i++) { _print(a + i * m, m); cerr << " "; } cerr << "]"; }
-template<typename T, typename ComparatorFn> void _print(oset<T, ComparatorFn> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// template<typename T, typename ComparatorFn> void _print(oset<T, ComparatorFn> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 // multiple printing
 template <class T, class... V> void _print(T t, V... v) {_print(t); if(sizeof...(v)) {cerr<<", "; _print(v...);}}
