194d6b3481c87f1eb87dea85e1d89140006b3cd4
Raghav Goel <raghavgoel1453@gmail.com>
2024-01-03 16:20:20 +0530

    wrote it more cleanly

--- templates/template.cpp
+++ templates/template.cpp
@@ -15,12 +15,12 @@ void solve() {
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	#ifndef ONLINE_JUDGE
-  freopen("io/input.txt", "r", stdin);
-  freopen("io/output.txt", "w", stdout);
-  freopen("io/error.txt", "w", stderr);
-  #endif
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("io/input.txt", "r", stdin);
+    freopen("io/output.txt", "w", stdout);
+    freopen("io/error.txt", "w", stderr);
+    #endif
 	
 	int t = 1;
 	cin >> t;
