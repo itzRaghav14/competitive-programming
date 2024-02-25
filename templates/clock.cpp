f202d81d0430d9c051edaadb14c055f30510ebde
Raghav Goel <raghavgoel1453@gmail.com>
2024-02-25 18:58:34 +0530

    updated clock

--- templates/clock.cpp
+++ templates/clock.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using namespace chrono;
-mt19937 rng(steady_clock::now().time_since_epoch().count());
-
 int main() {
   
-	auto start1 = high_resolution_clock::now();
+	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	auto start1 = chrono::high_resolution_clock::now();
 
 	// do something here
 
-    auto stop1 = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop1 - start1);
-    debug(Time, duration.count() / 1000);
+	auto stop1 = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop1 - start1);
+	debug(Time, duration.count() / 1000);
 
 	return 0;
 }
